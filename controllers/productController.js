1dcd4a615a1832bc5c4c0b4c3f5e7e7a6ad034e8
Pratiksha18Kshirsagar <kshirsagarpratiksha1820@gmail.com>
2025-12-19 19:34:16 +0530

    get with html

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,9 @@
 let productServices = require('../services/productServices');
+let path = require('path');
 
 let getAllProducts =(req, res)=> {
-  res.send(productServices.getAllProducts(req, res));
+  // res.send(productServices.getAllProducts(req, res));
+  res.sendFile(path.join(__dirname, '../view/products.html'));
 }
 
 let getProductById =(req, res)=> {
